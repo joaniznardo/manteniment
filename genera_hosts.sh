5ed44c636b3e8bfc42fc10a2af066642b77e1817
Joan Iznardo <joan.iznardo@a23pc00.jda.ins>
2017-03-20 10:48:45 +0100

    nom interficie automatic: la primera que troba

--- genera_hosts.sh
+++ genera_hosts.sh
@@ -3,6 +3,8 @@
 
 ## genera el fitxer de host per a ansible en llocs on (sic) no tenim assignació fixa d'ips
 ## importa la nomenclatura de la interficie i les macs que interessen
-sudo arp-scan -Nq --interface=enp2s0 --localnet |sed '1,2d' | sed '/^$/d' | sed '$d' | sed '$d' | sort -k2 | egrep "d8:cb|10:bf" | tee hosts20170228.csv | awk '{ print $1}' | tee ansible.hosts
+interficie=$(ip -o link show | awk '{print $2,$9}' | grep UP | head -n 1 | awk '{print $1}' | sed 's/://')
+##sudo arp-scan -Nq --interface=enp2s0 --localnet |sed '1,2d' | sed '/^$/d' | sed '$d' | sed '$d' | sort -k2 | egrep "d8:cb|10:bf" | tee hosts20170228.csv | awk '{ print $1}' | tee ansible.hosts
+sudo arp-scan -Nq --interface=$interficie --localnet |sed '1,2d' | sed '/^$/d' | sed '$d' | sed '$d' | sort -k2 | egrep "d8:cb|10:bf" | tee hosts20170228.csv | awk '{ print $1}' | tee ansible.hosts
 echo '[aula20]' | tee aula20.hosts
 cat ansible.hosts | tee -a aula20.hosts
